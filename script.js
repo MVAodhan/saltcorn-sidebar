04b2241e7959707a6b5425557574fe13fea0fad7
MVAodhan <hamilton.aodhan@gmail.com>
2021-12-16 14:06:45 +1300

    got rid of more css

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 $(document).ready(function () {
   $(".burger").on("click", function () {
     $("#sidebar").toggleClass("open");
-    $("#mNav").toggleClass("mOpen");
+    $("#mNav").toggleClass("d-block");
   });
   $(".burger-sidebar").on("click", function () {
     $("#sidebar").toggleClass("open");
-    $("#mNav").toggleClass("mOpen");
+    $("#mNav").toggleClass("d-block");
   });
 
   $(window).resize(function () {
-    if ($("#sidebar").hasClass("open") && $("#mNav").hasClass("mOpen")) {
-      $("#mNav").toggleClass("mOpen");
+    if ($("#sidebar").hasClass("open") && $("#mNav").hasClass("d-block")) {
+      $("#mNav").toggleClass("d-block");
     }
 
     if ($(window).width() <= 551 && $("#sidebar").hasClass("open")) {
       $("#sidebar").toggleClass("open");
-      $("#mNav").toggleClass("mOpen");
+      $("#mNav").toggleClass("d-block");
     }
 
-    if ($(window).width() > 551 && $("#mNav").hasClass("mOpen")) {
-      $("#mNav").toggleClass("nOpen");
+    if ($(window).width() > 551 && $("#mNav").hasClass("d-block")) {
+      $("#mNav").toggleClass("d-block");
       $("#sidebar").toggleClass("open");
     }
   });
